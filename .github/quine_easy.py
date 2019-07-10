be462ff334f32a98bc6896739965d37bcb47dbf7
Ben Wiederhake <BenWiederhake.GitHub@gmx.de>
2019-07-10 19:56:21 +0200

    Shorten to fit on a single screen

--- .github/quine_easy.py
+++ .github/quine_easy.py
@@ -1,68 +1,37 @@
 #!/usr/bin/env python3
-
-# A quine is a program that can output its own source code, and avoids
-# trivial means.  This program is a quine with a twist.
-# If you haven't already recognized the twist, I cannot help you.
+# A quine is a program that can output its own source code, and avoids trivial means.  This program is a quine with a twist.  If you haven't already recognized the twist, I cannot help you.
 
 import random
 
 # Things that really can't change.
-KEYWORDS = [
-    'append', 'def', 'dict', 'else', 'for', 'format', 'if', 'in',
-    'import', 'join', 'lower', 'n', 'print', 'python3', 'random',
-    'repr', 'return', 'upper', '#!/usr/bin/env',
-]
-
+KEYWORDS = ['def', 'dict', 'else', 'extend', 'for', 'format', 'if', 'in',  'import', 'join', 'lower', 'n', 'print', 'python3', 'random',  'repr', 'return', 'seed', 'upper', '#!/usr/bin/env']
 # This is where the quine-magic happens!
-MAGIC = '#!/usr/bin/env python3\n\n# A quine is a program that can output its own source code, and avoids\n# trivial means.  This program is a quine with a twist.\n# If you haven\'t already recognized the twist, I cannot help you.\n\nimport random\n\n# Things that really can\'t change.\nKEYWORDS = [\n    \'append\', \'def\', \'dict\', \'else\', \'for\', \'format\', \'if\', \'in\',\n    \'import\', \'join\', \'lower\', \'n\', \'print\', \'python3\', \'random\',\n    \'repr\', \'return\', \'upper\', \'#!/usr/bin/env\',\n]\n\n# This is where the quine-magic happens!\nMAGIC = {}\n\n# The same variable/function/whatever name needs to stay consistent between\n# definition and uses.  So make sure that during a single execution of this\n# program, only one case-flip is produced.\nTWISTS = dict()\n\n\ndef twist_word(word):\n    if word in KEYWORDS:\n        return word\n    if word in TWISTS:\n        new_word = TWISTS[word]\n    else:\n        new_word = \'\'.join(c.upper() if random.random() < 0.5 else c.lower() for c in word)\n        TWISTS[word] = new_word\n    return new_word\n\n\ndef twist(the_str):\n    words = []\n    curr_word = []\n    for c in the_str:\n        if c in \'.,:()[] \\\\\\n\\\'\':\n            words.append(\'\'.join(curr_word))\n            curr_word = []\n            words.append(c)\n        else:\n            curr_word.append(c)\n    words.append(\'\'.join(curr_word))\n    # We don\'t care about empty \'words\'.\n    # This can happen on \'\\n\\n\' and similar.\n    return \'\'.join(twist_word(word) for word in words)\n\n\n# Builds a quine from a "magic" string.  Note that skipping the `twist`\n# invocation would convert this into just a regular quine.\n# (With dead code.)\ndef quine_from(magic):\n    return magic.format(repr(magic))\n\n\ndef run():\n    twisted_magic = twist(MAGIC)\n    twisted_quine = quine_from(twisted_magic)\n    print(twisted_quine[:-1])\n\n\nrun()\n'
-
-# The same variable/function/whatever name needs to stay consistent between
-# definition and uses.  So make sure that during a single execution of this
-# program, only one case-flip is produced.
-TWISTS = dict()
+MAGIC = "#!/usr/bin/env python3\n# A quine is a program that can output its own source code, and avoids trivial means.  This program is a quine with a twist.  If you haven't already recognized the twist, I cannot help you.\n\nimport random\n\n# Things that really can't change.\nKEYWORDS = ['def', 'dict', 'else', 'extend', 'for', 'format', 'if', 'in',  'import', 'join', 'lower', 'n', 'print', 'python3', 'random',  'repr', 'return', 'seed', 'upper', '#!/usr/bin/env']\n# This is where the quine-magic happens!\nMAGIC = {}\nSEED = repr(random.random())\n\n\ndef twist_word(word):\n    if word in KEYWORDS:\n        return word\n    # The same variable/function/whatever name needs to stay consistent between definition and uses.  So make sure that during a single execution of this program, only one case-flip is produced.\n    random.seed(SEED + word)\n    random.random()\n    random.random()\n    new_word = ''.join(c.upper() if random.random() < 0.5 else c.lower() for c in word)\n    return new_word\n\n\ndef twist(the_str):\n    words, curr_word = [], []\n    for c in the_str:\n        if c in '.,:()[] \\\\\\n\\'':\n            words.extend([''.join(curr_word), c])\n            curr_word = []\n        else:\n            curr_word.extend(c)\n    # We don't care about empty 'words'.  This can happen on '\\n\\n' and similar.\n    return ''.join(twist_word(word) for word in words + [''.join(curr_word)])\n\n\ntwisted_magic = twist(MAGIC)\nprint(twisted_magic.format(repr(twisted_magic))[:-1])\n"
+SEED = '1337'
 
 
 def twist_word(word):
     if word in KEYWORDS:
         return word
-    if word in TWISTS:
-        new_word = TWISTS[word]
-    else:
-        random.seed('1337' + word)
-        random.random()
-        random.random()
-        new_word = ''.join(c.upper() if random.random() < 0.5 else c.lower() for c in word)
-        TWISTS[word] = new_word
+    # The same variable/function/whatever name needs to stay consistent between definition and uses.  So make sure that during a single execution of this program, only one case-flip is produced.
+    random.seed(SEED + word)
+    random.random()
+    random.random()
+    new_word = ''.join(c.upper() if random.random() < 0.5 else c.lower() for c in word)
     return new_word
 
 
 def twist(the_str):
-    words = []
-    curr_word = []
+    words, curr_word = [], []
     for c in the_str:
         if c in '.,:()[] \\\n\'':
-            words.append(''.join(curr_word))
+            words.extend([''.join(curr_word), c])
             curr_word = []
-            words.append(c)
         else:
-            curr_word.append(c)
-    words.append(''.join(curr_word))
-    # We don't care about empty 'words'.
-    # This can happen on '\n\n' and similar.
-    return ''.join(twist_word(word) for word in words)
+            curr_word.extend(c)
+    # We don't care about empty 'words'.  This can happen on '\n\n' and similar.
+    return ''.join(twist_word(word) for word in words + [''.join(curr_word)])
 
 
-# Builds a quine from a "magic" string.  Note that skipping the `twist`
-# invocation would convert this into just a regular quine.
-# (With dead code.)
-def quine_from(magic):
-    return magic.format(repr(magic))
-
-
-def run():
-    twisted_magic = twist(MAGIC)
-    twisted_quine = quine_from(twisted_magic)
-    print(twisted_quine[:-1])
-
-
-run()
+twisted_magic = twist(MAGIC)
+print(twisted_magic.format(repr(twisted_magic))[:-1])
--- QuInE.py
+++ QuInE.py
@@ -1,65 +1,37 @@
 #!/usr/bin/env python3
-
-# A QuInE iS a pROgRam thAt CAN OUtpUt ITS Own SoUrcE coDe, And AVOiDS
-# trIvial MEanS.  THiS pROgRam iS a QuInE wITH a twiST.
-# if yOu HAvEN'T aLREadY rECOGnIZeD ThE twiST, I caNnot help yOu.
+# A QuInE iS a pROgRam thAt CAN OUtpUt ITS Own SoUrcE coDe, And AVOiDS trIvial MEanS.  THiS pROgRam iS a QuInE wITH a twiST.  if yOu HAvEN'T aLREadY rECOGnIZeD ThE twiST, I caNnot help yOu.
 
 import random
 
 # thINgs thAt REallY CAN'T cHAnGE.
-KeyWOrdS = [
-    'append', 'def', 'dict', 'else', 'for', 'format', 'if', 'in',
-    'import', 'join', 'lower', 'n', 'print', 'python3', 'random',
-    'repr', 'return', 'upper', '#!/usr/bin/env',
-]
-
+KeyWOrdS = ['def', 'dict', 'else', 'extend', 'for', 'format', 'if', 'in',  'import', 'join', 'lower', 'n', 'print', 'python3', 'random',  'repr', 'return', 'seed', 'upper', '#!/usr/bin/env']
 # THiS iS wheRE ThE quINE-MAgiC hAPpENS!
-mAgic = '#!/usr/bin/env python3\n\n# A QuInE iS a pROgRam thAt CAN OUtpUt ITS Own SoUrcE coDe, And AVOiDS\n# trIvial MEanS.  THiS pROgRam iS a QuInE wITH a twiST.\n# if yOu HAvEN\'T aLREadY rECOGnIZeD ThE twiST, I caNnot help yOu.\n\nimport random\n\n# thINgs thAt REallY CAN\'T cHAnGE.\nKeyWOrdS = [\n    \'append\', \'def\', \'dict\', \'else\', \'for\', \'format\', \'if\', \'in\',\n    \'import\', \'join\', \'lower\', \'n\', \'print\', \'python3\', \'random\',\n    \'repr\', \'return\', \'upper\', \'#!/usr/bin/env\',\n]\n\n# THiS iS wheRE ThE quINE-MAgiC hAPpENS!\nmAgic = {}\n\n# tHe SAme VARIABLE/FunCtioN/WHateVEr namE nEeds To sTay CoNsistenT bETWEeN\n# defINItiOn And uses.  SO mAkE surE thAt durInG a SiNGLe Execution OF thIs\n# pROgRam, OnLy oNE Case-FliP iS pRODUcEd.\ntWIstS = dict()\n\n\ndef TWisT_wOrD(WORD):\n    if WORD in KeyWOrdS:\n        return WORD\n    if WORD in tWIstS:\n        NeW_woRd = tWIstS[WORD]\n    else:\n        NeW_woRd = \'\'.join(C.upper() if random.random() < 0.5 else C.lower() for C in WORD)\n        tWIstS[WORD] = NeW_woRd\n    return NeW_woRd\n\n\ndef twiST(The_stR):\n    woRds = []\n    cURr_wORd = []\n    for C in The_stR:\n        if C in \'.,:()[] \\\\\\n\\\'\':\n            woRds.append(\'\'.join(cURr_wORd))\n            cURr_wORd = []\n            woRds.append(C)\n        else:\n            cURr_wORd.append(C)\n    woRds.append(\'\'.join(cURr_wORd))\n    # WE don\'T CARE aBoUt EMpTy \'woRds\'.\n    # THiS CAN hAppeN on \'\\n\\n\' And SImiLAR.\n    return \'\'.join(TWisT_wOrD(WORD) for WORD in woRds)\n\n\n# bUilDS a QuInE FroM a "MagIC" StRiNG.  NOte thAt sKippInG ThE `tWIsT`\n# iNVOCation WOUld COnVeRT thIs iNTO juST a reGUlar QuInE.\n# (wiTh deAd coDe.)\ndef QUIne_frOM(mAgiC):\n    return mAgiC.format(repr(mAgiC))\n\n\ndef rUn():\n    twISTED_mAGiC = twiST(mAgic)\n    TWisTEd_QuiNe = QUIne_frOM(twISTED_mAGiC)\n    print(TWisTEd_QuiNe[:-1])\n\n\nrUn()\n'
-
-# tHe SAme VARIABLE/FunCtioN/WHateVEr namE nEeds To sTay CoNsistenT bETWEeN
-# defINItiOn And uses.  SO mAkE surE thAt durInG a SiNGLe Execution OF thIs
-# pROgRam, OnLy oNE Case-FliP iS pRODUcEd.
-tWIstS = dict()
+mAgic = "#!/usr/bin/env python3\n# A QuInE iS a pROgRam thAt CAN OUtpUt ITS Own SoUrcE coDe, And AVOiDS trIvial MEanS.  THiS pROgRam iS a QuInE wITH a twiST.  if yOu HAvEN'T aLREadY rECOGnIZeD ThE twiST, I caNnot help yOu.\n\nimport random\n\n# thINgs thAt REallY CAN'T cHAnGE.\nKeyWOrdS = ['def', 'dict', 'else', 'extend', 'for', 'format', 'if', 'in',  'import', 'join', 'lower', 'n', 'print', 'python3', 'random',  'repr', 'return', 'seed', 'upper', '#!/usr/bin/env']\n# THiS iS wheRE ThE quINE-MAgiC hAPpENS!\nmAgic = {}\nseed = repr(random.random())\n\n\ndef TWisT_wOrD(WORD):\n    if WORD in KeyWOrdS:\n        return WORD\n    # tHe SAme VARIABLE/FunCtioN/WHateVEr namE nEeds To sTay CoNsistenT bETWEeN defINItiOn And uses.  SO mAkE surE thAt durInG a SiNGLe Execution OF thIs pROgRam, OnLy oNE Case-FliP iS pRODUcEd.\n    random.seed(seed + WORD)\n    random.random()\n    random.random()\n    NeW_woRd = ''.join(C.upper() if random.random() < 0.5 else C.lower() for C in WORD)\n    return NeW_woRd\n\n\ndef twiST(The_stR):\n    woRds, cURr_wORd = [], []\n    for C in The_stR:\n        if C in '.,:()[] \\\\\\n\\'':\n            woRds.extend([''.join(cURr_wORd), C])\n            cURr_wORd = []\n        else:\n            cURr_wORd.extend(C)\n    # WE don'T CARE aBoUt EMpTy 'woRds'.  THiS CAN hAppeN on '\\n\\n' And SImiLAR.\n    return ''.join(TWisT_wOrD(WORD) for WORD in woRds + [''.join(cURr_wORd)])\n\n\ntwISTED_mAGiC = twiST(mAgic)\nprint(twISTED_mAGiC.format(repr(twISTED_mAGiC))[:-1])\n"
+seed = repr(random.random())
 
 
 def TWisT_wOrD(WORD):
     if WORD in KeyWOrdS:
         return WORD
-    if WORD in tWIstS:
-        NeW_woRd = tWIstS[WORD]
-    else:
-        NeW_woRd = ''.join(C.upper() if random.random() < 0.5 else C.lower() for C in WORD)
-        tWIstS[WORD] = NeW_woRd
+    # tHe SAme VARIABLE/FunCtioN/WHateVEr namE nEeds To sTay CoNsistenT bETWEeN defINItiOn And uses.  SO mAkE surE thAt durInG a SiNGLe Execution OF thIs pROgRam, OnLy oNE Case-FliP iS pRODUcEd.
+    random.seed(seed + WORD)
+    random.random()
+    random.random()
+    NeW_woRd = ''.join(C.upper() if random.random() < 0.5 else C.lower() for C in WORD)
     return NeW_woRd
 
 
 def twiST(The_stR):
-    woRds = []
-    cURr_wORd = []
+    woRds, cURr_wORd = [], []
     for C in The_stR:
         if C in '.,:()[] \\\n\'':
-            woRds.append(''.join(cURr_wORd))
+            woRds.extend([''.join(cURr_wORd), C])
             cURr_wORd = []
-            woRds.append(C)
         else:
-            cURr_wORd.append(C)
-    woRds.append(''.join(cURr_wORd))
-    # WE don'T CARE aBoUt EMpTy 'woRds'.
-    # THiS CAN hAppeN on '\n\n' And SImiLAR.
-    return ''.join(TWisT_wOrD(WORD) for WORD in woRds)
+            cURr_wORd.extend(C)
+    # WE don'T CARE aBoUt EMpTy 'woRds'.  THiS CAN hAppeN on '\n\n' And SImiLAR.
+    return ''.join(TWisT_wOrD(WORD) for WORD in woRds + [''.join(cURr_wORd)])
 
 
-# bUilDS a QuInE FroM a "MagIC" StRiNG.  NOte thAt sKippInG ThE `tWIsT`
-# iNVOCation WOUld COnVeRT thIs iNTO juST a reGUlar QuInE.
-# (wiTh deAd coDe.)
-def QUIne_frOM(mAgiC):
-    return mAgiC.format(repr(mAgiC))
-
-
-def rUn():
-    twISTED_mAGiC = twiST(mAgic)
-    TWisTEd_QuiNe = QUIne_frOM(twISTED_mAGiC)
-    print(TWisTEd_QuiNe[:-1])
-
-
-rUn()
+twISTED_mAGiC = twiST(mAgic)
+print(twISTED_mAGiC.format(repr(twISTED_mAGiC))[:-1])
